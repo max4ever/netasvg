c3a6d15691e1db2963b064c86fd6baaa933648c3
Maxim Postoronca <maxim.postoronca@gmail.com>
2019-04-03 18:39:05 +0200

    added svg custom response

---
+++ src/Svg/SvgResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Svg;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class SvgResponse extends Response
+{
+
+    private const HTML_TEMPLATE = '<?xml version="1.0" standalone="no"?><svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">%s</svg>';
+
+    public function __construct($content = '', int $status = 200, array $headers = [])
+    {
+        parent::__construct($content, $status, $headers);
+        $this->headers->set('Content-Type', 'image/svg+xml');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function sendContent()
+    {
+        echo sprintf(self::HTML_TEMPLATE, $this->content);
+
+        return $this;
+    }
+}
